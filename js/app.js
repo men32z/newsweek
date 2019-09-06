fe236ffb9533800bc24684ddb055afa8286fb207
Luis Preza <preza.luis@gmail.com>
2019-09-06 11:49:21 -0500

    dinamic date with js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+(()=>{
+  //document ready
+  //getting the date
+  var date = new Date();
+  var date_options = {year:'numeric', weekday:'short', month:'short', day: '2-digit'};
+  document.getElementById('date-today').innerHTML = date.toLocaleDateString('en-US', date_options);
+})();
