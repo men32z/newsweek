c0d660581da135a03dcd89dfc0dd4557e58dc537
Luis Preza <preza.luis@gmail.com>
2019-09-21 14:00:54 -0500

    firs version finished

--- js/app.js
+++ js/app.js
@@ -5,4 +5,15 @@
   var date = new Date();
   var date_options = {year:'numeric', weekday:'short', month:'short', day: '2-digit'};
   document.getElementById('date-today').innerHTML = date.toLocaleDateString('en-US', date_options);
+
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 162) {
+          $("header.basic-container").addClass("sticky");
+      } else {
+          $("header.basic-container").removeClass("sticky");
+      }
+  });
+
 })();
